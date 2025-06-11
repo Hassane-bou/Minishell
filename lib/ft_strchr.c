99c849cd36a06386764ce198bd53cee1434bedf2
Radouane Mouafik <rmouafik@c2r9p6.1337.ma>
2025-06-11 11:46:52 +0100

    hh

--- lib/ft_strchr.c
+++ lib/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rmouafik <rmouafik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 18:06:48 by rmouafik          #+#    #+#             */
-/*   Updated: 2025/06/11 09:56:11 by rmouafik         ###   ########.fr       */
+/*   Updated: 2025/06/11 11:27:43 by rmouafik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_strchr(const char *s, int c)
 	{
 		if (*s == b)
 		{
-			return ((char *)s + 1);
+			return ((char *)s);
 		}
 		s++;
 	}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rmouafik <rmouafik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 10:35:46 by rmouafik          #+#    #+#             */
-/*   Updated: 2025/06/11 10:42:34 by rmouafik         ###   ########.fr       */
+/*   Updated: 2025/06/11 11:29:06 by rmouafik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@ int main(int ac, char *av[], char **envp)
 	int		i;
 	int		pos;
 	t_env	**env_copy;
+	char	*str_value;
 
 	i = 0;
 	while (envp[i])
 		i++;
-	env_copy = malloc(sizeof(t_env **) * i);
+	env_copy = malloc(sizeof(t_env *) * i);
 	i = 0;
 	while (envp[i])
 	{
-		env_copy[i]->value = ft_strdup(ft_strchr(envp[i], '=') + 1);
-		pos = env_copy[i]->value - envp[i];
-		env_copy[i]->key = ft_substr(envp[i], 0, pos - 1);
+		env_copy[i] = malloc(sizeof(t_env));
+		str_value = ft_strchr(envp[i], '=');
+		pos = str_value - envp[i];
+		env_copy[i]->key = ft_substr(envp[i], 0, pos);
+		env_copy[i]->value = ft_strdup(str_value + 1);
 		printf("%s=%s\n", env_copy[i]->key, env_copy[i]->value);
 		i++;
 	}
