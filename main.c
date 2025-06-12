d74fc5f0ac4faf5b7b8e8af1228bdfecb74826d1
Radouane Mouafik <rmouafik@c2r8p6.1337.ma>
2025-06-12 10:38:59 +0100

    hh

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rmouafik <rmouafik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 10:35:46 by rmouafik          #+#    #+#             */
-/*   Updated: 2025/06/12 10:26:20 by rmouafik         ###   ########.fr       */
+/*   Updated: 2025/06/12 10:38:21 by rmouafik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,26 +60,41 @@ void	env_add_back(t_env **env_list, t_env *new_node)
 	}
 }
 
-int main(int ac, char *av[], char **envp)
-{
-	int		i;
-	int		pos;
-	t_env	*env_head;
-	t_env	*env_node;
-	char	*ptr_value;
+// int main(int ac, char *av[], char **envp)
+// {
+// 	int		i;
+// 	int		pos;
+// 	t_env	*env_head;
+// 	t_env	*env_node;
+// 	char	*ptr_value;
 
-	i = 0;
-	env_head = NULL;
-	while (envp[i])
-	{
-		env_node = malloc(sizeof(t_env));
-		ptr_value = ft_strchr(envp[i], '=');
-		pos = ptr_value - envp[i];
-		env_node->key = ft_substr(envp[i], 0, pos);
-		env_node->value = ft_strdup(ptr_value + 1);
-		env_node->next = NULL;
-		env_add_back(&env_head, env_node);
-		printf("%s=%s\n", env_node->key, env_node->value);
-		i++;
-	}
+// 	i = 0;
+// 	env_head = NULL;
+// 	while (envp[i])
+// 	{
+// 		env_node = malloc(sizeof(t_env));
+// 		ptr_value = ft_strchr(envp[i], '=');
+// 		pos = ptr_value - envp[i];
+// 		env_node->key = ft_substr(envp[i], 0, pos);
+// 		env_node->value = ft_strdup(ptr_value + 1);
+// 		env_node->next = NULL;
+// 		env_add_back(&env_head, env_node);
+// 		printf("%s=%s\n", env_node->key, env_node->value);
+// 		i++;
+// 	}
+// }
+int main()
+{
+    char *input;
+    
+    while(1)
+    {
+        input = readline("Minishell> ");
+        if(input == NULL)
+            break;
+        if(*input)
+            add_history(input);
+    }
+    printf("%s\n",input);
+    // free(input);
 }
